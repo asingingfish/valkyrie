744b3c3fb7b9c9ef21972a9fde0a2502b930330f
yuyiming <yuyiming@made-in-china.com>
2018-05-20 13:41:53 +0800

    append home

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <home/>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Home from "./components/home";
+
+    export default {
+        name: 'App',
+
+        components: {
+            Home
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        componentName: "Home",
+
+        components: {},
+
+        props: {},
+
+        data() {
+            return {};
+        },
+
+        computed: {},
+        watch: {},
+
+        mounted() {
+        },
+        created() {
+        },
+
+        methods: {},
+
+
+    }
+</script>
+
+
+<style lang="scss">
+
+</style>
